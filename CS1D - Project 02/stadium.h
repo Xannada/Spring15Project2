ea700b9bdd73c5e718ce7bc73bdbfdd3f97a270e
zebramints <zebramints@gmail.com>
2015-05-03 20:42:55 -0700

    Graph class

---
+++ CS1D - Project 02/stadium.h
@@ -0,0 +1,90 @@
+#include "Project 02.h"
+
+class stadium
+{
+
+
+	public:
+	stadium();
+
+	void addStadium(string sName,
+					string tName,
+					string a1,
+					string a2,
+					string tele,
+					string date,
+					string seats,
+					bool astroTurf,
+					bool national);
+
+	void getList();
+
+	void alphaLeague();
+	void alphaStadium();
+	void alphaTeam();
+	void Opening();
+	void seating();
+
+	void setStadiumName(string compare, string sName);
+	void setTeamName(string compare, string tName);
+	void setAddress1(string compare, string a1);
+	void setAddress2(string compare, string a2);
+	void setPhone(string compare, string tele);
+	void setOpening(string compare, string date);
+	void setCapacity(string compare, string seats);
+	void setFieldType(string compare, bool astroTurf);
+	void setLeague(string compare, bool national);
+
+	string getStadiumName(string compare);
+	string getTeamName(string compare);
+	string getAddress1(string compare);
+	string getAddress2(string compare);
+	string getPhone(string compare);
+	string getOpening(string compare);
+	string getCapacity(string compare);
+	bool getFieldType(string compare);
+	bool getLeague(string compare);
+
+	private:
+
+
+		stadiumNode *comparison(string compare)
+		{
+			stadiumNode *tempNode;
+
+			tempNode = stadiumList;
+
+			while(tempNode->stadiumName != compare)
+			{
+				tempNode = tempNode->next;
+			}
+
+			return tempNode;
+		}
+
+		stadiumNode *copy(stadiumNode *list)
+		{
+			if(list == NULL)
+			{
+				return NULL;
+			}
+
+			stadiumNode *outputList = new stadiumNode;
+			outputList->stadiumName = list->stadiumName;
+			outputList->teamName = list->teamName;
+			outputList->address1 = list->address1;
+			outputList->address2 = list->address2;
+			outputList->phone = list->phone;
+			outputList->opening = list->opening;
+			outputList->capacity = list->capacity;
+			outputList->fieldType = list->fieldType;
+			outputList->league = list->league;
+			outputList->prev = list->prev;
+			outputList->next = copy(list->next);
+
+			return outputList;
+
+		}
+
+		stadiumNode *stadiumList;
+};
